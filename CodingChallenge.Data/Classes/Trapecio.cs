763ad4436142348f4775cb10bb24fd7d05e26bd0
Facundo Arce <facundo.ea07@gmail.com>
2023-08-29 13:12:56 -0300

    Add trapecio

---
+++ CodingChallenge.Data/Classes/Trapecio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Data.Classes
+{
+    public class Trapecio : FormaGeometrica
+    {
+
+        public Trapecio(decimal baseMayor, decimal baseMenor, decimal altura, decimal ladoA, decimal ladoB) 
+            : base(new Dictionary<string, decimal>
+                {
+                    { "BaseMayor", baseMayor },
+                    { "BaseMenor", baseMenor },
+                    { "Altura", altura },
+                    { "LadoA", ladoA },
+                    { "LadoB", ladoB }
+                })
+        { }
+
+        public override decimal CalcularArea() => (Medidas["BaseMayor"] + Medidas["BaseMenor"]) * Medidas["Altura"] / 2;
+        public override decimal CalcularPerimetro() => Medidas["BaseMayor"] + Medidas["BaseMenor"] + Medidas["LadoA"] + Medidas["LadoB"];
+    }
+}
