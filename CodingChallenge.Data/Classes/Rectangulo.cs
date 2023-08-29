5aa67ba97af4878a04566a3b6ca6a36c41103a61
Facundo Arce <facundo.ea07@gmail.com>
2023-08-29 15:11:00 -0300

    Add Rectangulo

---
+++ CodingChallenge.Data/Classes/Rectangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge.Data.Classes
+{
+    public class Rectangulo : FormaGeometrica
+    {
+        private decimal _base { get; set; }
+        private decimal _altura { get; set; }
+
+        public Rectangulo(decimal baseRectangulo, decimal alturaRectangulo) 
+        {
+            _base = baseRectangulo;
+            _altura = alturaRectangulo;
+        }
+
+        public override decimal CalcularArea() => _base * _altura;
+        public override decimal CalcularPerimetro() => 2 * (_base + _altura);
+    }
+}
